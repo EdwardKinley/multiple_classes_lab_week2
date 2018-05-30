454a4956e27e604ba02960c61037d3049e23e6de
jamos90 <jamos_98@hotmail.com>
2018-05-30 15:40:09 +0100

    final commit

--- pub.rb
+++ pub.rb
@@ -8,11 +8,23 @@ class Pub
     @name = name
     @till_amount = till_amount
     @drinks = drinks
-
   end
 
   def pub_stocks_drink?(drink)
     return @drinks.include?(drink)
+  end
+
+  def stock_level
+    return @drinks.values.sum
+    # stock_count = 0
+    # for drink in @drinks.
+    #   stock_count += drink.values
+    # end
+    # return stock_count
+  end
+
+  def stock_value
+    return @drinks.values.sum * @drinks[:price]
   end
 
 end
--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -5,13 +5,13 @@ require_relative("../drink.rb")
 class PubTest < MiniTest::Test
 
   def setup
-    @drink1 = Drink.new("beer", 5)
-    @drink2 = Drink.new("wine", 4)
-    @drink3 = Drink.new("gin", 6)
-    @drink4 = Drink.new("vodka", 7)
-    @drink5 = Drink.new("cider", 8)
-    @drink6 = Drink.new("milkshake", 3)
-    @pub1 = Pub.new("Chanter", 100, {@drink1, @drink2, @drink3, @drink4, @drink5, @drink6})
+    @drink1 = Drink.new("beer", 5, 4)
+    @drink2 = Drink.new("wine", 4, 17)
+    @drink3 = Drink.new("gin", 6, 30)
+    @drink4 = Drink.new("vodka", 7, 30)
+    @drink5 = Drink.new("cider", 8, 5)
+    @drink6 = Drink.new("milkshake", 3, 0)
+    @pub1 = Pub.new("Chanter", 100, {@drink1 => 10, @drink2 => 10, @drink3 => 10, @drink4 => 10, @drink5 => 10, @drink6 => 10})
   end
 
   def test_pub_name
@@ -28,6 +28,14 @@ class PubTest < MiniTest::Test
 
   def test_pub_stocks_drink?
     assert_equal(true, @pub1.pub_stocks_drink?(@drink1))
+  end
+
+  def test_stock_level
+    assert_equal(60, @pub1.stock_level)
+  end
+
+  def test_stock_value
+    assert_equal(330, @pub1.stock_value)
   end
 
 end
